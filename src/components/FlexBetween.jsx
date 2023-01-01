6fff9440c90799a3ac4239337dd3a06fbdc1f35a
chamarabandarawd <chamarabandarawd@gmail.com>
2023-01-01 17:49:03 +0530

    nva bar done

---
+++ src/components/FlexBetween.jsx
@@ -0,0 +1,10 @@
+const { styled } = require("@mui/system");
+const { Box } = require("@mui/material");
+
+
+const FlexBetween =  styled(Box)({
+    display:"flex",
+    alignItems:"center"
+});
+
+export default FlexBetween;
